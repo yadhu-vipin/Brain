eb128ef2ba48f06d93f49bbf8eb297d4e2af3912
unknown <yadhuvipin12@gmail.com>
2025-03-15 08:15:09 +0530

    done

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"YBEE8hh0bz5bzzKTSH6CDqXZNa1e2fXwtWDNX+DKbcM=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
